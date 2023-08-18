9f31a38cfcfcc8f440662d0a4a9c46b87f9bdecc
Tatiana Deeva <deewa.tatjana@yandex.ru>
2023-08-18 03:37:23 +0300

    (cypress) test constructor

---
+++ cypress/e2e/constructor/locators.ts
@@ -0,0 +1,12 @@
+export const locators = {
+    INGREDIENTS_COL: '[class*=burger-ingredients_column]',
+    CONSTRUCTOR_COL: '[class*=burger-constructor_column]',
+    BUTTON: '.button',
+    INGREDIENT_CARD: '[class*=ingredient-card_body]',
+    CONSTRUCTOR_ITEM: '.constructor-element',
+    BUNS_DIV: '#buns',
+    SAUCES_DIV: '#sauces',
+    MAINS_DIV: '#mains',
+    MODAL_CARD: '[class*=modal_card]',
+    ORDER_NUMBER: '[class*=order-details_number]',
+};
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+        login(): Chainable<any>;
+      }
+};
+
+Cypress.Commands.add('login', () => {
+    cy.visit('/login');
+
+    cy.get('input[name="email"]').type('cat@cat.com');
+    cy.get('input[name="password"]').type('testpass');
+    cy.contains('button', 'Вход').click();
+ });
